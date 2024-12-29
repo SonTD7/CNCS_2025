2241e6ec9ac8411fa7a5d8855fde1c0638cedb50
sonmvsah <sonmv.sah@gmail.com>
2024-12-29 16:39:27 +0700

    update Case 3

--- components/themes/default/header/mobile-lang.tsx
+++ components/themes/default/header/mobile-lang.tsx
@@ -1,9 +1,9 @@
 "use client"
 import { useRouter } from "next/navigation";
 import Cookies from "js-cookie"
-import { Vie } from "../languages/vi";
+import { Vi } from "../languages/vi";
 import { Us } from "../languages/us";
-import  {Jp}  from "../languages/ja";
+import  {Ja}  from "../languages/ja";
 
 import DarkMode from "../darkmode/dark-mode";
 
@@ -26,13 +26,13 @@ export default function MobileLang() {
     return (
         <div className="flex justify-between space-x-1">
             <a className="mx-2 p-1 border rounded-full hover:ring-1 ring-red-500 hover:bg-gradient-to-tr hover:from-slate-300 hover:to-red-200" href="/" onClick={() => handleLocaleChange("vi")}>
-                <Vie />
+                <Vi />
             </a>
             <a className="mx-2 p-1 border rounded-full hover:ring-1 ring-cyan-500 hover:bg-gradient-to-tr hover:from-slate-300 hover:to-cyan-200" href="/en" onClick={() => handleLocaleChange("en")}>
                 <Us />
             </a>
             <a className="mx-2 p-1 border rounded-full hover:ring-1 ring-cyan-500 hover:bg-gradient-to-tr hover:from-slate-300 hover:to-cyan-200" href="/ja" onClick={() => handleLocaleChange("ja")}>
-                <Jp />
+                <Ja />
             </a>
             <DarkMode />
         </div>
