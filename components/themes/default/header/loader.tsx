433db5674824046f3bd11cc5cfb124c177a3540a
SONTD <sontddeveloper@gmail.com>
2025-11-22 23:23:49 +0700

    add props for loader to build deploy

--- components/themes/default/header/loader.tsx
+++ components/themes/default/header/loader.tsx
@@ -1,14 +1,12 @@
 "use client";
+
 import { useState, useEffect } from "react";
 import { cn } from "@/lib/utils/utils";
 
-interface LoaderProps {
-  locale: string;
-  dicts: any;
-}
-
-export default function Loader({ locale, dicts }: LoaderProps) {
+export default function Loader() {
   const [isLoading, setIsLoading] = useState(true);
+  const locale =
+    typeof window !== "undefined" ? window.navigator.language : "en";
 
   useEffect(() => {
     const handleLoading = () => setIsLoading(false);
@@ -29,18 +27,7 @@ export default function Loader({ locale, dicts }: LoaderProps) {
       )}
     >
       <div className="relative flex flex-col items-center">
-        {/* Logo Image */}
-        <img
-          src="/logo.png" // Đường dẫn logo của bạn
-          alt="Logo"
-          className="w-80 animate-float"
-        />
-
-        {/* Spinner raindrop */}
-        {/* <div className="absolute top-1/2 animate-spin">
-                    <div className="w-6 h-6 border-4 border-t-[#66c8cc] border-[#b2e6e8] rounded-full"></div>
-                </div> */}
-
+        <img src="/logo.png" alt="Logo" className="w-80 animate-float" />
         <p
           className="mt-8 text-lg font-semibold tracking-wide relative overflow-hidden
   bg-gradient-to-r from-[#66c8cc] via-[#66c8cc] to-transparent
